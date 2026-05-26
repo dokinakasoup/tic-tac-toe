f6c560becf455584d9b96d5c926fd2660ffcea71
Ashwani <ashwanipratap2905@gmail.com>
2026-05-26 15:19:07 +0530

    fixed heading

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="title">Tic-Tac-Toe</h1>
+    {/* <h1 className="title">Tic-Tac-Toe</h1> */}
     <App />
   </StrictMode>,
 )
